40b27f3ae174242331b89c6add0059a429d7dd86
Christian Valesares <cvalesares@gmail.com>
2022-02-23 16:46:02 -0500

    add migrations and models for tea/customer/subscription

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_211616) do
+ActiveRecord::Schema.define(version: 2022_02_23_214134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 2022_02_23_211616) do
 
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
-    t.integer "price"
-    t.string "status"
+    t.float "price"
+    t.integer "status", default: 0
     t.integer "frequency"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
