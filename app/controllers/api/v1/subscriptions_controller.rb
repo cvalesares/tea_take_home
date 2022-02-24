05d6cc607d416c6b8ba4cbc01d3bcc5dc6e04937
Christian Valesares <cvalesares@gmail.com>
2022-02-23 19:22:17 -0500

    add subscription#update endpoint

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -5,7 +5,16 @@ class Api::V1::SubscriptionsController < ApplicationController
     if subscription.save
       render json: SubscriptionSerializer.new(Subscription.find(subscription.id)), status: 201
     else
-      render json: { errors: { details: "There was an error creating this request" } }, status: 400
+      render json: { errors: { details: "There was an error creating this subscription" } }, status: 400
+    end
+  end
+
+  def update
+    subscription = Subscription.update(params[:id], subscription_params)
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription)
+    else
+      render json: { errors: { details: "There was an error updating this request" } }, status: 400
     end
   end
 
